ed39b384ae3c61d6158946109da6ac7cc5490204
johan hamidi <johanhamdidu38@gmail.com>
2018-10-12 17:16:31 +0200

    jeu donnees candidat RH

---
+++ jeuDonnéeRhCandidat.sql
@@ -0,0 +1,10 @@
+INSERT INTO CANDIDAT(IdPersonne, nom, Prenom, numeroTel, loginPersonne, motDePasse)
+VALUES
+("3","dupond","benjamin","0626060606","bdupond","benjamin"),
+("4","dupand","george","0636060606","gdupand","george"),
+("5","dupind","lee","0646060606","ldupind","lee");
+
+INSERT INTO RH(IdPersonne, nom, Prenom, numeroTel, loginPersonne, motDePasse)
+VALUES
+("1","durand","jhon","0606060660","jdurand","jhon"),
+("2","dupont","pat","0616060606","pdupond","pat");
