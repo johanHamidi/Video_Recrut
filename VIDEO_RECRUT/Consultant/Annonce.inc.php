28aada124f7e392585c92d6f100e691e83c903ee
DeveloppeurA <benard.theo.sio@gmail.com>
2018-10-19 14:40:43 +0200

    connexion bd

---
+++ VIDEO_RECRUT/Consultant/Annonce.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+
+foreach ($ as $key => $value) {
+  // code...
+}
+ ?>
---
+++ VIDEO_RECRUT/db.inc.php
@@ -0,0 +1,3 @@
+<?php
+$connexion = mysqli_connect("localhost","root","root","video_recrut");
+$res = mysqli_select_db($connexion,'test');
